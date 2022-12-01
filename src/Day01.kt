cb56fd573d174b4f8338bcf36a1c392911e7ae23
Tristan Shawn den Ouden <trisdo687@gmail.com>
2022-12-02 00:28:19 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,41 @@
 fun main() {
+
+    fun getInventories(input: List<String>): MutableList<Int> {
+        val calories: MutableList<Int> = mutableListOf()
+        var currentInventory = 0
+
+        input.forEach { line ->
+            if (line == "") {
+                calories.add(currentInventory)
+                currentInventory = 0
+            } else {
+                currentInventory += line.toInt()
+            }
+        }
+
+        if (currentInventory != 0) {
+            calories.add(currentInventory)
+        }
+
+        return calories
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val calories = getInventories(input)
+        return calories.maxOrNull() ?: 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = getInventories(input)
+        calories.sortDescending()
+        return calories[0] + calories[1] + calories[2]
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+     val input = readInput("Day01")
+     println(part1(input))
+     println(part2(input))
 }
